b68b9454e2a5a11be490439ec90607a942e0923b
goertzm0 <s6mngoer@uni-bonn.de>
2026-06-04 14:17:01 +0200

    Initial Spring Boot Setup

---
+++ src/main/java/com/mg/bewerbungstracker/BewerbungstrackerApplication.java
@@ -0,0 +1,13 @@
+package com.mg.bewerbungstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BewerbungstrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BewerbungstrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mg/bewerbungstracker/BewerbungstrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mg.bewerbungstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BewerbungstrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
